39240c8783091ce4410548245e70e58c334dc3fc
rosero97 <jarosero82@misena.edu.co>
2022-08-28 21:36:26 -0500

    se realizo el modelo vista controlador para las mesas pero no deja agregarlas aun

---
+++ controlador/mesas_con.php
@@ -0,0 +1,21 @@
+<?php
+include('../modelo/mesas_mod.php');
+$obj = new num_menu();
+if($_POST){
+
+    $obj->mesa = $_POST['mesa'];
+}
+
+if(isset($_POST['guarda'])){
+    $obj->agregar();
+}
+
+if(isset($_POST['modifica'])){
+    $obj->modificar();
+}
+
+if(isset($_POST['elimina'])){
+    $obj->eliminar();
+}
+
+?>
---
+++ modelo/mesas_mod.php
@@ -0,0 +1,35 @@
+<?php
+    class num_menu{
+
+                    public $mesa;
+                    
+
+                    function agregar(){
+                                        $c = $conn ;
+                                        $query = "select * from num_menu where num_mesa = '$this->mesa'";
+                                        $ejecuta = mysqli_query($c, $query);
+                                        if(mysqli_fetch_array($ejecuta)){
+                                           echo "<script> alert('La Mesa ya Existe en el Sistema')</script>";
+                                        }else{
+                                           $insertar = "insert into num_menu value(
+                                                                                    '$this->mesa'
+                                                                                    
+                                           )";
+                                           echo $insertar;
+                                           mysqli_query($c,$insertar);
+                                           echo "<script> alert('La Mesa fue Creada en el Sistema')</script>";
+                                            
+                                        }
+                    }
+
+                    function modificar(){
+
+                    }   
+                    
+                    function eliminar(){
+
+                    }
+
+                    
+    }
+?>
