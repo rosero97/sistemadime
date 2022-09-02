a1a7f25cc01566a79b4bc7891f5e3e33eac109fc
rosero97 <jarosero82@misena.edu.co>
2022-09-02 00:01:50 -0500

    se modifico modelo hay que verificar

--- modelo/mesas_mod.php
+++ modelo/mesas_mod.php
@@ -27,22 +27,6 @@ class num_menu{
                     }
 
                     function modificar(){
-                     $obj = new conexion();
-                                        $c=$obj->conectando();
-                                        $query = "select * from num_menu where num_mesa = '$this->mesa'";
-                                        $ejecuta = mysqli_query($c, $query);
-                                        if(mysqli_fetch_array($ejecuta)){
-                                           echo "<script> alert('La Mesa ya Existe en el Sistema')</script>";
-                                        }else{
-                                           $insertar = "insert into num_menu value(
-                                                                                    '$this->mesa'
-                                                                                    
-                                           )";
-                                           echo $insertar;
-                                           mysqli_query($c,$insertar);
-                                           echo "<script> alert('La Mesa fue Creada en el Sistema')</script>";
-                                            
-                                        }
 
                     }   
                     
