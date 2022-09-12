42ae7d7b8cab513cfba7753ac4c04210fc216ef1
rosero97 <92414029+rosero97@users.noreply.github.com>
2022-09-12 14:21:16 -0500

    se arreglo para el envio de datos del cliente a la base de datos ya funciona

--- modelo/registro_usuario.php
+++ modelo/registro_usuario.php
@@ -1,6 +1,6 @@
 <?php
 
- include("../../conexion/conectar.php");
+include("../conexion/conectar.php");
 
 $conet = new Conexion();
 $con = $conet->conectando();   
@@ -26,6 +26,13 @@ if(isset($_POST['Registrarse'])){
 
         $consulta3="INSERT INTO `cliente` (`t_cliente`, `ide_cliente`) VALUES (NULL, '$numerodoc')";
 
+        $verificar_correo = mysqli_query($con, "SELECT * FROM persona WHERE correo='$correo'");
+
+        if(mysqli_num_rows ($verificar_correo) >0 ){
+            echo "<script 'text/javascript'>alert('El correo esta registrado, porfavor intente con otro.'); window.location.href='../vista/3.RegistrarseA.php';</script>";
+            exit();
+        }
+        
         $resultado=mysqli_query($con, $consulta);
         $resultado2=mysqli_query($con, $consulta2);
         $resultado3=mysqli_query($con, $consulta3);
@@ -39,7 +46,7 @@ if(isset($_POST['Registrarse'])){
     }
     else{
         
-        echo "<script 'text/javascript'>alert('Verifique las contraseñas'); window.location.href='../vista/3.RegistrarseA.php';</script>";
+        echo "<script 'text/javascript'>alert('Verifique las contraseñas'); window.location.href='../vista/4.RegistrarseC.php';</script>";
     }
 }
 ?>
