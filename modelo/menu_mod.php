b6b748719c2fb7a6eba0d71b49d856d8b55eb160
rosero97 <92414029+rosero97@users.noreply.github.com>
2022-09-13 17:03:47 -0500

    se creo el modelo funcional de menu

---
+++ modelo/menu_mod.php
@@ -0,0 +1,93 @@
+<?php
+   
+    
+class menu{
+
+                  public $num_menu;
+                  public $nombre;
+                  public $imagen;
+                  public $descripcion;
+                  public $ida;
+
+                    
+
+                    function agregar(){
+                       
+                                          $obj = new conexion();
+                                          $c=$obj->conectando();
+                                          $query = "select * from menu where nombre = '$this->nombre'";
+                                          $ejecuta = mysqli_query($c, $query);
+                                          if(mysqli_fetch_array($ejecuta)){
+                                             echo "<script> alert('El plato ya Existe en el Sistema')</script>";
+                                          }else{
+                                          $conet = new Conexion();
+                                          $c = $conet->conectando(); 
+                                          $ruta = '../imagenes/'.$_FILES['imagen']['name'];
+                                          move_uploaded_file($_FILES['imagen']['tmp_name'],$ruta);
+                                          $insertar = "insert into menu value(
+                                                                                    '$this->num_menu',
+                                                                                    '$this->nombre',
+                                                                                    '$ruta',
+                                                                                    '$this->descripcion',
+                                                                                    '$this->ida'
+                                                                                    
+                                          )";
+                                          echo $insertar;
+                                          mysqli_query($c,$insertar);
+                                          echo "<script> alert('El plato fue Creado en el Sistema')</script>";
+                                            
+                                        }
+                    }
+
+                    function modificar(){
+                                          $obj = new conexion();
+                                          $c=$obj->conectando();
+                                          $query = "select * from menu where nombre = '$this->nombre'";
+                                          $ejecuta = mysqli_query($c, $query);
+                                          if(mysqli_fetch_array($ejecuta)){
+                                             echo "<script> alert('El plato ya Existe en el Sistema')</script>";
+                                          }else{
+                                             $conet = new Conexion();
+                                             $c = $conet->conectando(); 
+                                             $ruta = '../imagenes/'.$_FILES['imagen']['name'];
+                                             move_uploaded_file($_FILES['imagen']['tmp_name'],$ruta);
+                                             $update = "update menu set
+                                                                                    num_menu='$this->num_menu',
+                                                                                    nombre='$this->nombre',
+                                                                                    imagen='$ruta',
+                                                                                    descripcion='$this->descripcion',
+                                                                                    ide_encargado='$this->ida',
+                                                                                    where num_menu='$this->num_menu'
+                                                                                    
+                                             ";
+                                             echo $update;
+                                             mysqli_query($c,$update);
+                                             echo "<script> alert('El plato fue modificado en el sistema')</script>";
+                                             
+                                          }
+
+
+                    }   
+                    
+                    function eliminar(){
+                                          $obj = new conexion();
+                                          $c=$obj->conectando();
+                                          $query = "select * from menu where id_menu = '$this->nombre'";
+                                          $ejecuta = mysqli_query($c, $query);
+                                          if(mysqli_fetch_array($ejecuta)){
+                                             echo "<script> alert('La Mesa NO se puede eliminar del Sistema, solo se puede modificar')</script>";
+                                          }else{
+                                             $update = "delete from menu 
+                                                                                    where id_menu='$this->num_menu'
+                                             ";
+                                             echo $update;
+                                             mysqli_query($c,$update);
+                                             echo "<script> alert('La mesa fue Eliminada del sistema'); window.location.href='../vista/menu2.php';</script>";
+                                             
+                                          }
+
+                    }
+
+                    
+    }
+?>
