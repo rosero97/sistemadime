dc20d6524f8c65621ef57617730b08bbd1f820db
CarlosAfanador2 <92995615+CarlosAfanador2@users.noreply.github.com>
2022-11-17 16:15:02 -0500

    Update Iniciar Sesion-2.php
    Se sube el el inicio en php con los estilos nuevos

--- vista/Iniciar Sesion-2.php
+++ vista/Iniciar Sesion-2.php
@@ -5,40 +5,41 @@ include("../modelo/iniciar_sesion.php")
 ?>
 
 <!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Iniciar Sesión</title>
-    <link rel="stylesheet" href="../config/style1.css">
-        <div class="hola mundo">
-            <center><img src="../imagenes/digital-.png" alt=""></center>
+<html lang="en">
+    <head>
+        <div class="form">
+            <h2>Digital's Menu</h2>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Menu de Navegación</title>
+        <link rel="stylesheet" href="../config/style1.css">
+   <body>
         </div>
-        
-    </div>
-    <header>
-        <div class="menu">
-            <nav>
-                <ul>
-                    <li><a href="../index.html">Inicio</a></li>
-                    <li><a href="login/1.Tipo_Registro.html">Registrarse</a></li>
-                    <li><a href="Iniciar Sesion-2.php">Iniciar Sesion</a></li>
-                    <li><a href="inicio/Ayuda.html">ayuda</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="../config/login.css">
-</head><!DOCTYPE html>
-<html lang="es">
-<body>
-    <section>
+        <header class="header">
+            <div class="container">
+                <div class="btn-menu">
+                    <label for="btn-menu">☰</label>
+        </header>
+        <div class="capa">
+            <input type="checkbox" id="btn-menu">
+            <div class="container-menu">
+                <div class="cont-menu">
+                    <section>   
+                    <nav>   
+                        <a href="../index.html">Inicio</a>
+                        <a href="login/1.Tipo_Registro.html">Registrarse</a>
+                        <a href="Iniciar Sesion-2.php.html">Iniciar Sesion</a>
+                        <a href="inicio/Ayuda.html">Ayuda</a>
+                    </nav>
+                </section>
+                
+    </body>
+            </div>
+            </div>
+        </header>
+        <section>
+        <link rel="stylesheet" href="../config/styles_tiniciar.css">
         <div class="color"></div>
         <div class="color"></div>
         <div class="color"></div>
@@ -48,30 +49,27 @@ include("../modelo/iniciar_sesion.php")
             <div class="square" style="--i:2;"></div>
             <div class="square" style="--i:3;"></div>
             <div class="square" style="--i:4;"></div>
-            <div class="container">
+            <div class="contenedor">
                 <div class="form">
                     <h2>Iniciar Sesión</h2>
                     <form action="../modelo/iniciar_sesion.php" method="post">
                     <form action="administrador.html" method="post">
+                    <form action="cliente1.html">
                         <div class="inputBox">
-                            <input type="email" name="correo" placeholder="Correo electronico" required/>
+                            <input type="email" placeholder="Correo electronico" required/>
                         </div>
                         <div class="inputBox">
-                            <input type="password" name="contraseña" placeholder="Contraseña" required/>
+                            <input type="password" placeholder="Contraseña" required/>
                         </div>
                         <div class="inputBox">
-                            <input type="submit" name="iniciar" value="Iniciar Sesion" />
+                            <input type="submit" value="Iniciar Sesion" />
                         </div>
                         <p class="forget">
-                            ¿Has olvidado tu contraseña? <a href="login/recuperar.html">Recuperar contraseña</a>
+                            ¿Has olvidado tu contraseña? <a href="recuperar.html">Recuperar contraseña</a>
                         </p>
-
-                        </p>
-                    </form>
-                </div>
             </div>
         </div>
     </section>
-</body>
-
 </html>
+</html>
+
