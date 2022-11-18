b22e150db7f376a2d689a1069ab8376851105faf
Camilo90088 <92952604+Camilo90088@users.noreply.github.com>
2022-11-18 17:04:00 -0500

    se arreglo el menu desplegable

--- vista/menu.php
+++ vista/menu.php
@@ -220,6 +220,6 @@ if(isset($_POST['search'])){
             $('body').bootstrapMaterialDesign();
         });
     </script>
-    <script src="../js/main.js"></script>
+    <script src="./js/main.js"></script>
 </body>
 </html>
