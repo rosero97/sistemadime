169caea6da58bb3a9307f01e42efce947b23dffb
rosero97 <jarosero82@misena.edu.co>
2023-02-17 12:20:45 -0500

    Se modifico la encriptacion de la contraseña pero falta verificarla

--- modelo/cambio_mod.php
+++ modelo/cambio_mod.php
@@ -15,26 +15,28 @@ class cambio{
                                         $c=$obj->conectando();
                                         $query = "select * from persona where correo = '$this->correoRe'";
                                         $ejecuta = mysqli_query($c, $query);
+                                        $fila = mysqli_fetch_assoc($ejecuta);
                                         if(mysqli_fetch_array($ejecuta)){
-                                            if($this->contrasena==$this->contrasena1){
-                                                $update = "update persona set
-                                                                                    correo='$this->correoRe',
-                                                                                    contrasena='$this->contrasena'
-                                                                                    where correo='$this->correoRe'
-
-                                                ";
-                                                echo $update;
-                                                mysqli_query($c,$update);
-                                                echo "<script> alert('La contraseña fue modificada en el sistema'); window.location.href='../vista/Iniciar Sesion-2.php';</script>";
-                                            }else{
-                                                echo "<script> alert('Las contraseñas no son iguales'); window.location.href='../vista/Cambio de contraseña.php';</script>";
-                                            }
-                                        }else{
                                             echo "<script> alert('El correo actual no coincide con la base de datos'); window.location.href='../vista/Cambio de contraseña.php';</script>";
+                                        }else{
+                                            if(password_verify($this->contrasena, $fila['contrasena'])){
+                                                if($this->contrasena==$this->contrasena1){
+                                                    $contra_fuerte = password_hash ($this->contrasena, PASSWORD_DEFAULT);
+                                                    $update = "update persona set
+                                                                                        correo='$this->correoRe',
+                                                                                        contrasena='$contra_fuerte'
+                                                                                        where correo='$this->correoRe'
+    
+                                                    ";
+                                                    echo $update;
+                                                    mysqli_query($c,$update);
+                                                    echo "<script> alert('La contraseña fue modificada en el sistema'); window.location.href='../vista/Iniciar Sesion-2.php';</script>";
+                                                }else{
+                                                    echo "<script> alert('Las contraseñas no son iguales'); window.location.href='../vista/Cambio de contraseña.php';</script>";
+                                                }
+                                        
+                                            }
                                         }
-
-                    }
-
-
-    }
+                                    }
+}
 ?>
