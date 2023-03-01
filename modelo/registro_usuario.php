a5102e6a526cd174425be45aded41e22f4d3f9f8
Camilo90088 <92952604+Camilo90088@users.noreply.github.com>
2023-03-01 16:47:31 -0500

    se arreglo el captcha

--- modelo/registro_usuario.php
+++ modelo/registro_usuario.php
@@ -25,35 +25,35 @@ if(isset($_POST['Registrarse'])){
 
     if(!$atributos['success']){
         echo "<script 'text/javascript'>alert('El captcha es obligatorio'); window.location.href='../vista/4.RegistrarseC.php';</script>";
-    }
+    }else{
+        $tipodoc=mysqli_real_escape_string($con, $_POST['tipodoc']);
+        $correo=mysqli_real_escape_string($con, $_POST['correo']);
+        $contraseña=mysqli_real_escape_string($con, $_POST['contraseña']);
 
-    $tipodoc=mysqli_real_escape_string($con, $_POST['tipodoc']);
-    $correo=mysqli_real_escape_string($con, $_POST['correo']);
-    $contraseña=mysqli_real_escape_string($con, $_POST['contraseña']);
-        
-    if($contraseña==$contraseña2){
-        $contra_fuerte = password_hash ($contraseña, PASSWORD_DEFAULT);
-        $consulta="INSERT INTO persona (id_persona, nombre_completo, correo, celular, contrasena, fcod_tipo_doc, rolid) VALUES ('$numerodoc', '$nombres', '$correo', '$numerocel', '$contra_fuerte', '$tipodoc', '$roleid')";
+        if($contraseña==$contraseña2){
+            $contra_fuerte = password_hash ($contraseña, PASSWORD_DEFAULT);
+            $consulta="INSERT INTO persona (id_persona, nombre_completo, correo, celular, contrasena, fcod_tipo_doc, rolid) VALUES ('$numerodoc', '$nombres', '$correo', '$numerocel', '$contra_fuerte', '$tipodoc', '$roleid')";
 
-        $verificar_correo = mysqli_query($con, "SELECT * FROM persona WHERE correo='$correo'");
+            $verificar_correo = mysqli_query($con, "SELECT * FROM persona WHERE correo='$correo'");
 
-        if(mysqli_num_rows ($verificar_correo) >0 ){
-            echo "<script 'text/javascript'>alert('El correo esta registrado, porfavor intente con otro.'); window.location.href='../vista/4.RegistrarseC.php';</script>";
-            exit();
+            if(mysqli_num_rows ($verificar_correo) >0 ){
+                echo "<script 'text/javascript'>alert('El correo esta registrado, porfavor intente con otro.'); window.location.href='../vista/4.RegistrarseC.php';</script>";
+                exit();
+            }
+
+            $resultado=mysqli_query($con, $consulta);
+            if($resultado){
+
+                echo "<script 'text/javascript'>alert('El usuario ha sido registrado'); window.location.href='../vista/login/5.Confirmacion.html';</script>";
+
+            }else{
+                die("Ha habido un error, verifique nuevamente:".mysqli_error($con));
+            }
         }
-        
-        $resultado=mysqli_query($con, $consulta);
-        if($resultado){
+        else{
 
-            echo "<script 'text/javascript'>alert('El usuario ha sido registrado'); window.location.href='../vista/login/5.Confirmacion.html';</script>";
-
-        }else{
-            die("Ha habido un error, verifique nuevamente:".mysqli_error($con));
+            echo "<script 'text/javascript'>alert('Verifique las contraseñas'); window.location.href='../vista/4.RegistrarseC.php';</script>";
         }
-    }
-    else{
-        
-        echo "<script 'text/javascript'>alert('Verifique las contraseñas'); window.location.href='../vista/4.RegistrarseC.php';</script>";
     }
 }
 ?>
