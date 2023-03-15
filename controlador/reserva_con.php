5753e25d6fe932c931b48c932a544e3f75df612f
Camilo90088 <92952604+Camilo90088@users.noreply.github.com>
2023-03-15 16:24:31 -0500

    se arreglo el controlador de la reserva

--- controlador/reserva_con.php
+++ controlador/reserva_con.php
@@ -6,6 +6,8 @@ if($_POST){
 
     $obj->reserva = $_POST['reserva'];
     $obj->fecha = $_POST['fecha'];
+    $obj->hora_inicio = $_POST['hora'];
+    $obj->hora_fin = date('H:i', strtotime($obj->hora_inicio . ' +1 hour'));
     $obj->observaciones = $_POST['observaciones'];
     $obj->mesa = $_POST['mesa'];
     $obj->personas = $_POST['personas'];
