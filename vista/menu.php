702c4e48285355a3515d6605e709655219e7cc74
Camilo90088 <92952604+Camilo90088@users.noreply.github.com>
2023-03-22 17:31:30 -0500

    se arreglo el responsive del modal del usuario

--- vista/menu.php
+++ vista/menu.php
@@ -150,7 +150,9 @@ $restau = mysqli_fetch_array($consul);
                                     do{  
                             ?>
                         <div class="table-responsive menu_usuario">
-                            <img class="img_menu" src="<?php echo $arreglo2 [2];?>" alt="">
+			    <div class="img_menu">
+                         	<img src="<?php echo $arreglo2 [2];?>" alt="">
+			    </div>
                             <div class="descripcion_menu">
                                 <h5><?php echo $arreglo2 [1]?></h5>
                                 <p>Descripción</p>
@@ -158,9 +160,11 @@ $restau = mysqli_fetch_array($consul);
                                 <div id="myModal-<?php echo $arreglo2 ['id_menu']?>" class="modal">
                                   <div class="modal-content" style="width: 80%;">
                                     <span id="close-modal-btn-<?php echo $arreglo2 ['id_menu']?>" class="close">&times;</span>
-                                    <img class="img_menu" src="<?php echo $arreglo2 [2]?>" alt="">
-                                    <h2 style="position: absolute; left: 45%; top: 10%;"><?php echo $arreglo2 [1]?></h2>
-                                    <p style="position: absolute; left: 45%; top: 120px;"><?php echo $arreglo2 [3]?></p>
+				    <div class="modal_img">
+                                    	<img src="<?php echo $arreglo2 [2]?>" alt="">
+				    </div>
+                                    <h2 class="nombre_menu"><?php echo $arreglo2 [1]?></h2>
+                                    <p class="des_menu"><?php echo $arreglo2 [3]?></p>
                                   </div>
                                 </div>
                             </div>
