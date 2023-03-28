92c9ba096d3d6b81cf3dad76760b0e5e7e7ea732
Camilo90088 <92952604+Camilo90088@users.noreply.github.com>
2023-03-28 11:39:56 -0500

    Update modifica_reserva_con.php

--- controlador/modifica_reserva_con.php
+++ controlador/modifica_reserva_con.php
@@ -5,7 +5,10 @@ $obj = new reserva();
 if($_POST){
 
     $obj->reserva = $_POST['reserva'];
+    $obj->mesa = $_POST['mesa'];
     $obj->fecha = $_POST['fecha'];
+    $obj->hora_inicio = $_POST['hora'];
+    $obj->hora_fin = date('H:i', strtotime($obj->hora_inicio . ' +1 hour'));
     $obj->observaciones = $_POST['observaciones'];
 }
 
